192906e472c43b90c2e2fc7647de6c594f7c326b
alek <wait82985@gmail.com>
2024-09-25 13:27:54 +0500

    initial commit

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void test1(){
+        String[] words = {"abc","ab","bc","b"};
+        int[] expected = {5,4,3,2};
+        int[] actual = new Solution().sumPrefixScores(words);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void test2(){
+        String[] words = {"abcd"};
+        int[] expected = {4};
+        int[] actual = new Solution().sumPrefixScores(words);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        String[] words = {"abc","ab","bc","b"};
+        int[] expected = {5,4,3,2};
+        int[] actual = new Solution().sumPrefixScores(words);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
